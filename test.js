af7403ba97af858226731cad283283a99276d069
siva <siva@spotdraft.com>
2022-06-28 21:01:05 +0530

    test

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ const RegExpFromString = require('regexp-from-string');
 const regExp = RegExpFromString("/\[([A-Z]+\-[0-9]+)\]/i");
 // `regExp` is the same as RegExp(/Rock/ig);
 console.log(regExp);
+
+siva
