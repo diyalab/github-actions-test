b73d316f51b3c4a35033b96c7b7d78372ba583b7
siva <siva@spotdraft.com>
2022-07-12 12:37:04 +0530

    test

--- scripts/test/test.sh
+++ scripts/test/test.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "value is $JIRA_APP_URL"
+set -v #echo on
+set -e #fail on error
+mkdir -p /root/.ssh
+echo "$GITLAB_SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
+ssh-keyscan -H gitlab.dev.spotdraft.com > /root/.ssh/known_hosts
+chmod 600 /root/.ssh/id_rsa
+apk add --no-cache git && apk add yq
+yq --version
+git config --global user.name "siva"
+git config --global user.email "siva@spotdraft.com"
+git clone --single-branch --branch master git@gitlab.dev.spotdraft.com:sd/argo-manifests.git
