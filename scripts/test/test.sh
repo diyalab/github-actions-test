24d6af15f6b2db1199e49ed7d9f71b2e85df8c80
siva <siva@spotdraft.com>
2022-07-12 12:44:25 +0530

    Update test.sh

--- scripts/test/test.sh
+++ scripts/test/test.sh
@@ -2,6 +2,7 @@
 set -v #echo on
 set -e #fail on error
 # mkdir -p /root/.ssh
+mkdir -p ~/.ssh/
 echo "$GITLAB_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 ssh-keyscan -H gitlab.dev.spotdraft.com > /root/.ssh/known_hosts
 chmod 600 /root/.ssh/id_rsa
