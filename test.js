c462b8f751f6a7687cdb1959709bdc559193f4b8
siva273 <siva@spotdraft.com>
2022-08-09 10:31:36 +0000

    Bors 2 (#52)
    Co-authored-by: siva273 <cvaganesh.v@gmail.com>

--- test.js
+++ test.js
@@ -4,4 +4,6 @@ const regExp = RegExpFromString("/\[([A-Z]+\-[0-9]+)\]/i");
 // `regExp` is the same as RegExp(/Rock/ig);
 console.log(regExp);
 
-siva ganesh 123  124 123 456
+
+siva ganesh 123  124 123 456 456 566
+
